0c7812b3a71a47f0fa07fa83d9de2532598c0ea5
Calvin Wu <calvinwu4@gmail.com>
2024-02-23 18:52:05 -0800

    add 'filename_hash' column

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,5 +1,6 @@
 const os = require('os')
 const db = require('db')
+const crypto = require('crypto')
 
 // SQLite usage
 // https://github.com/WiseLibs/better-sqlite3
@@ -11,6 +12,40 @@ function buildRoutes (router) {
   })
 
   router.get('/api/files', async (req, res) => {
+    const instance = db.instance
+
+    // Check if the filename_hash column exists in the table schema
+    const schema = instance.prepare("PRAGMA table_info('files')").all()
+    const columnExists = schema.some((column) => column.name === 'filename_hash')
+
+    // If the column does not exist, add it to the table schema
+    if (!columnExists) {
+      instance.prepare(`
+        ALTER TABLE files ADD COLUMN filename_hash VARCHAR(64)
+      `).run()
+    }
+
+    // Create an index for the filename_hash column
+    instance.prepare(`
+      CREATE INDEX IF NOT EXISTS idx_filename_hash ON files (filename_hash)
+    `).run()
+
+    // Select all rows from the files table
+    const rows = instance.prepare(`
+      SELECT id, filename FROM files WHERE filename_hash IS NULL
+    `).all()
+
+    // Update the filename_hash column for each row
+    for (const row of rows) {
+      const { id, filename } = row
+      const hash = crypto.createHash('sha256').update(filename).digest('hex')
+      
+      // Update the filename_hash column with the calculated hash value
+      instance.prepare(`
+        UPDATE files SET filename_hash = ? WHERE id = ?
+      `).run(hash, id)
+    }
+  
     const files = db.instance
       .prepare(`
         SELECT * FROM files
@@ -24,9 +59,9 @@ function buildRoutes (router) {
     const newFile = db.instance
       .prepare(`
         INSERT INTO files
-          (description, filename, mimetype, src)
+          (description, filename, mimetype, src, filename_hash)
           VALUES
-          (@description, @filename, @mimetype, @src)
+          (@description, @filename, @mimetype, @src, @filename_hash)
           RETURNING *
       `)
       .get({
@@ -34,6 +69,7 @@ function buildRoutes (router) {
         filename: file.name,
         mimetype: file.mimetype,
         src: file.base64,
+        filename_hash: crypto.createHash('sha256').update(file.name).digest('hex'),
       })
     return res.send(newFile)
   })
