33189c32e7e505f12c740d2745e291cc6c7bd1ab
Calvin Wu <calvinwu4@gmail.com>
2024-02-27 18:13:14 -0800

    added more tests

--- test/server/routes/index.test.js
+++ test/server/routes/index.test.js
@@ -65,7 +65,7 @@ describe('routes', function () {
       expect(res.body.filename).to.equal('elvis(1).jpg')
     })
 
-    it('if kitten.jpg, kitten(1).jpg, and kitten(2).jpg exists, inputting kitten.jpg should set its name to kitten(3).jpg', async function () {
+    it('if kitten.jpg, kitten(1).jpg, and kitten(2).jpg exist, inputting kitten.jpg should set its name to kitten(3).jpg', async function () {
       const input = {
         description: 'test',
         file: {
@@ -105,6 +105,68 @@ describe('routes', function () {
       req.body = input
       await router.postRoutes[route](req, res)
       expect(res.body.filename).to.equal('kitten(1)(1).jpg')
+    })
+
+    it('if dog(2).jpg exists and dog.jpg and dog(1).jpg have been added, inputting dog.jpg should change its name to dog(3).jpg', async function () {
+      const preinput = {
+        description: 'test',
+        file: {
+          name: 'dog.jpg',
+          mimetype: 'image/jpg',
+          base64: 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==',
+        },
+      }
+      req.body = preinput
+      await router.postRoutes[route](req, res)
+
+      const preinput2 = {
+        description: 'test',
+        file: {
+          name: 'dog(1).jpg',
+          mimetype: 'image/jpg',
+          base64: 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==',
+        },
+      }
+      req.body = preinput2
+      await router.postRoutes[route](req, res)
+
+
+      const input = {
+        description: 'test',
+        file: {
+          name: 'dog.jpg',
+          mimetype: 'image/jpg',
+          base64: 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==',
+        },
+      }
+      req.body = input
+      await router.postRoutes[route](req, res)
+      expect(res.body.filename).to.equal('dog(3).jpg')
+    })
+
+    it('if dog(2).jpg exists and dog(2)(1).jpg has been added, inputting dog(2).jpg should change its name to dog(2)(2).jpg', async function () {
+      const preinput = {
+        description: 'test',
+        file: {
+          name: 'dog(2)(1).jpg',
+          mimetype: 'image/jpg',
+          base64: 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==',
+        },
+      }
+      req.body = preinput
+      await router.postRoutes[route](req, res)
+
+      const input = {
+        description: 'test',
+        file: {
+          name: 'dog(2).jpg',
+          mimetype: 'image/jpg',
+          base64: 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==',
+        },
+      }
+      req.body = input
+      await router.postRoutes[route](req, res)
+      expect(res.body.filename).to.equal('dog(2)(2).jpg')
     })
   })
 })
