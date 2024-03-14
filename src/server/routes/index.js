e3d075c4c96de10a804a894e856354e352708fde
ccornali611 <ccornali_coding@gmx.com>
2024-03-14 17:23:37 -0600

    Updated: impleted using FileRepository on endpoints

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,39 +1,33 @@
 const db = require('db')
+const FileRepository = require('db/repositories/fileRepository')
 
 // SQLite usage
 // https://github.com/WiseLibs/better-sqlite3
 // https://github.com/WiseLibs/better-sqlite3/blob/master/docs/api.md
 
 function buildRoutes (router) {
+  const fileRepository = new FileRepository(db.instance)
+
   router.get('/api/username', async (req, res) => {
     return res.send({ username: req.username })
   })
 
   router.get('/api/files', async (req, res) => {
-    const files = db.instance
-      .prepare(`
-        SELECT * FROM files
-      `)
-      .all() // use .get() to fetch a single row
+    const files = fileRepository.getUserFiles({ username: req.username })
     return res.send(files)
   })
 
   router.post('/api/files', async (req, res) => {
+    const username = req.username
     const { description, file } = req.body
-    const newFile = db.instance
-      .prepare(`
-        INSERT INTO files
-          (description, filename, mimetype, src)
-          VALUES
-          (@description, @filename, @mimetype, @src)
-          RETURNING *
-      `)
-      .get({
-        description,
-        filename: file.name,
-        mimetype: file.mimetype,
-        src: file.base64,
-      })
+
+    const newFile = fileRepository.insertFile({
+      description,
+      filename: file.name,
+      mimetype: file.mimetype,
+      src: file.base64,
+      username,
+    })
     return res.send(newFile)
   })
 
